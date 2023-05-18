008ec9b573f2c532309ab0f7b34a8a5c6186d79b
ptan01 <ptanjim01@gmail.com>
2023-05-18 10:32:26 +0600

    gallery section done

---
+++ src/pages/home/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../header/Header';
+import Gallery from '../gallery/Gallery';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Gallery></Gallery>
+        </div>
+    );
+};
+
+export default Home;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../leyOut/Main";
+import Home from "../pages/home/home/Home";
 
 const router = createBrowserRouter([
     {
@@ -8,7 +9,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                
+                element: <Home></Home>
             }
         ]
     }
